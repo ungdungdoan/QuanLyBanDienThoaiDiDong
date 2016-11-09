4114f5e62247785657aa87e0d0ec2b9fd2db0fbb
ungdungdoan <ungdungdoan@gmail.com>
2016-11-09 21:12:24 +0700

    Quoc Thang create Class Application

---
+++ QuanLyBanDienThoaiDiDong/src/ui/Application.java
@@ -0,0 +1,11 @@
+package ui;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FrmNhaSanXuat frm1 = new FrmNhaSanXuat();
+		 frm1.setVisible(true);
+	}
+
+}
